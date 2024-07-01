ff0c963e110f2ce8c2c30979692422913a336fcb
purekite <soocy0718@gmail.com>
2024-07-02 01:26:29 +0900

    :memo: 7월 1일 리트코드 한 문제 완료

---
+++ LeetCode/SolutionTwoSum.java
@@ -0,0 +1,13 @@
+class SolutionTwoSum {
+	public int[] twoSum(int[] nums, int target) {
+		int[] twoNum = new int[2];
+		for(int i = 0; i < nums.length; i++){
+			for(int j = i + 1; j < nums.length; j++) {
+				if(nums[i] + nums[j] == target){
+					twoNum = new int[]{i, j};
+				}
+			}
+		}
+		return twoNum;
+	}
+}
