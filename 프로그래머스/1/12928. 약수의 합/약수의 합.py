1bc7468ba3fa4744935e99f5ed28ff0e717cb971
순수연 <48740214+PureKite@users.noreply.github.com>
2026-01-04 10:56:50 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,9 +1,7 @@
 import math
 def solution(n):
     answer = 0
-    for i in range(1, int(math.sqrt(n)) + 1):
+    for i in range(1, n // 2 + 1):
         if n % i == 0:
             answer += i
-            if(n // i != i):
-                answer += n // i
-    return answer
+    return answer + n
